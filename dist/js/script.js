06486f263587dcca6045ce343e2a4d6ac7f817a7
isterya <vash.vybor.1@gmail.com>
2024-05-08 21:45:11 +0200

    about me is done

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,26 @@ document.addEventListener('DOMContentLoaded', () => {
    const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       closeElem = document.querySelector('.menu__close'),
-      overlay = document.querySelector('.menu__overlay');
+      overlay = document.querySelector('.menu__overlay'),
+      linkStyle = document.querySelectorAll('.sidepanel__link svg path'),
+      textMenu = document.querySelectorAll('.sidepanel__text span'),
+      menuDivider = document.querySelectorAll('.sidepanel__divider');
+
+   const handleScroll = () => {
+      const yOffset = window.pageYOffset;
+
+      linkStyle.forEach((el) => {
+         el.style.fill = yOffset >= 500 ? 'black' : '';
+      });
+
+      textMenu.forEach((el) => {
+         el.style.color = yOffset >= 300 ? 'black' : '';
+      });
+
+      menuDivider.forEach((el) => {
+         el.style.backgroundColor = yOffset >= 400 ? 'black' : '';
+      });
+   };
 
    const closeMenu = () => {
       menu.classList.remove('active');
@@ -14,4 +33,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
    closeElem.addEventListener('click', closeMenu);
    overlay.addEventListener('click', closeMenu);
+
+   document.addEventListener('scroll', () => {
+      requestAnimationFrame(handleScroll);
+   });
 });
